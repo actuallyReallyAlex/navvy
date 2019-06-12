0cc6c3959d2631706bfc50e7d6e1c5ddb85f41c6
Alex Lee <alex@alex-lee.site>
2019-06-12 13:14:43 -0700

    Fix tests for Links.

--- src/tests/Sidebar/LinkCollapsibleChild.test.js
+++ src/tests/Sidebar/LinkCollapsibleChild.test.js
@@ -18,4 +18,11 @@ describe('<LinkCollapsibleChild />', () => {
     const component = shallow(<LinkCollapsibleChild {...defaultProps} />)
     expect(component).toMatchSnapshot()
   })
+
+  test('Should call the link handler if the link is clicked.', () => {
+    const component = shallow(<LinkCollapsibleChild {...defaultProps} />)
+    expect(defaultProps.handler).toHaveBeenCalledTimes(0)
+    component.find('WithTheme(Button)').simulate('click')
+    expect(defaultProps.handler).toHaveBeenCalledTimes(1)
+  })
 })
--- src/tests/Sidebar/LinkStandard.test.js
+++ src/tests/Sidebar/LinkStandard.test.js
@@ -17,8 +17,36 @@ describe('<LinkStandard />', () => {
     defaultProps.setSelected.mockClear()
   })
 
-  test('Should render the <LinkStandard /> component.', () => {
+  test('Should render the <LinkStandard /> component as a non-selected Link with sidebar expanded.', () => {
     const component = shallow(<LinkStandard {...defaultProps} />)
     expect(component).toMatchSnapshot()
+  })
+
+  test('Should render the <LinkStandard /> component as a non-selected Link with sidebar collapsed.', () => {
+    const component = shallow(
+      <LinkStandard {...defaultProps} size="collapsed" />
+    )
+    expect(component).toMatchSnapshot()
+  })
+
+  test('Should render the <LinkStandard /> component as a selected Link with sidebar expanded.', () => {
+    const component = shallow(
+      <LinkStandard {...defaultProps} selected="Title" />
+    )
+    expect(component).toMatchSnapshot()
+  })
+
+  test('Should render the <LinkStandard /> component as a selected Link with sidebar collapsed.', () => {
+    const component = shallow(
+      <LinkStandard {...defaultProps} selected="Title" size="collapsed" />
+    )
+    expect(component).toMatchSnapshot()
+  })
+
+  test('Should call the link handler if the link is clicked.', () => {
+    const component = shallow(<LinkStandard {...defaultProps} />)
+    expect(defaultProps.handler).toHaveBeenCalledTimes(0)
+    component.find('WithTheme(Button)').simulate('click')
+    expect(defaultProps.handler).toHaveBeenCalledTimes(1)
   })
 })
